f19a76ad7aa7c06a301e7694acdf5373814f553d
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2019-01-02 21:58:50 -0600

    Touch up the code a little

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client()
+const client = new Discord.Client();
 const giveMeAJoke = require('give-me-a-joke');
 const config = require("./config.js");
 
@@ -7,31 +7,56 @@ client.on('ready', function() {
     console.log('Logged in as: ' + client.username + " - (" + client.id + ")");
 });
 
+
+// The command list
+let commands = {
+	help: (message, str) =>
+	{
+		message.channel.send("There is no help for the helpless...");
+	},
+	joke: (message, str) =>
+	{
+		giveMeAJoke.getRandomDadJoke(joke => message.channel.send(joke));
+	}
+}
+
+
+/**
+ * Attempts to run a command from the command list.
+ * @param {String} cmd The command to be executed
+ * @param {*} message The message object received from Discord
+ * @param {*} str The message string (with any pre-processing you've performed on it).
+ */
+function runCommand(cmd, message, str)
+{
+	if(typeof commands[cmd] !== "undefined")
+	{
+		return commands[cmd](message, str)
+	}
+	else
+	{
+		// unknown command
+	}
+}
+
+
 // When chat messages are received
 client.on("message", message => {
 	if (message.author.bot) return;
-	var str = message.content;
-	//replies dank to any image link, needs to be confined to a channel...
-	var m = str.replace(/[^a-zA-Z ]/g, "").toLowerCase();
-	if(m.includes("im ") ){
-		message.channel.send("Hi" + m.split("im").pop() + ", I'm Dad!");
-	}
-	
-    if (str.substring(0, 1) == "!"){ 
-	// if message starts with "!"
-        var command = str.substring(1); 
-		// store the command for cleaner code/reading
+	let str = message.content;
 
-        if(command == "help"){
-            message.channel.send("There is no help for the helpless...");
-		}
-		if(command == "joke"){
-			giveMeAJoke.getRandomDadJoke (function(joke) {
-				message.channel.send(joke);
-			});
-		}
+	//replies dank to any image link, needs to be confined to a channel...
+	let modified = ' ' + str.toLowerCase().replace(/i am/g, 'im').replace(/[^a-z ]/g, '');
+	if(modified.indexOf(' im ') != -1) {
+		message.channel.send(`Hi ${modified.split(' im ').map(i=>i.trim()).filter(i=>i).join(' ')}, I'm Dad!`);
+	}
+
+	// if message starts with "!"
+    if (str.startsWith("!")) { 
+		// store the command for cleaner code/reading
+		let command = str.substring(1); 
+        runCommand(command, message, str);
     }
-	
 });
 
 client.login(config.token);
