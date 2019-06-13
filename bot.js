4a629dd63d3579dc3bc3f615a691da4c9f6d34d7
Luke Hardin <lhardin491@gmail.com>
2019-06-12 19:43:30 -0500

    removed joke dependency

--- bot.js
+++ bot.js
@@ -1,21 +1,18 @@
-const giveMeAJoke = require('give-me-a-joke');
+const request = require('request');
 
-function init(client)
-{
-	client.on('ready', function() {
+function init(client) {
+	client.on('ready', function () {
 		console.log(`Logged in as: ${client.username} - (${client.id})`);
 	});
 
 
 	// The command list
 	let commands = {
-		help: (message, str) =>
-		{
+		help: (message, str) => {
 			message.channel.send("There is no help for the helpless...");
 		},
-		joke: (message, str) =>
-		{
-			giveMeAJoke.getRandomDadJoke(joke => message.channel.send(joke));
+		joke: (message, str) => {
+			getRandomDadJoke(joke => message.channel.send(joke));
 		}
 	}
 
@@ -26,18 +23,29 @@ function init(client)
 	 * @param {*} message The message object received from Discord
 	 * @param {*} str The message string (with any pre-processing you've performed on it).
 	 */
-	function runCommand(cmd, message, str)
-	{
-		if(typeof commands[cmd] !== "undefined")
-		{
+	function runCommand(cmd, message, str) {
+		if (typeof commands[cmd] !== "undefined") {
 			return commands[cmd](message, str)
 		}
-		else
-		{
+		else {
 			// unknown command
 		}
 	}
 
+	function getRandomDadJoke(joke) {
+		var options = {
+			url: 'https://icanhazdadjoke.com/',
+			headers: {
+				'Accept': 'application/json'
+			}
+		};
+		request(options, function (err, response, body) {
+			if(!err && response.statusCode === 200){
+				body = JSON.parse(body);
+				return joke(body.joke);
+			}
+		});
+	}
 
 	// When chat messages are received
 	client.on("message", message => {
@@ -46,32 +54,30 @@ function init(client)
 
 		//replies dank to any image link, needs to be confined to a channel...
 		let modified = str
-		.toLowerCase()
-		.replace(/i am/g, 'im')
-		.replace(/[^a-z\.\?\! ]/g, '')
-		.split(/\.|\?|\!/)
-		.map(i=>
-		{ 
-			i = ' ' + i
-			let start = i.indexOf(' im ')
-			if(start === -1)
-			{
-				return 
-			}
-			return i.substr(start)
-		})
-		.filter(i=>i)
-		.join(' and ')
+			.toLowerCase()
+			.replace(/i am/g, 'im')
+			.replace(/[^a-z\.\?\! ]/g, '')
+			.split(/\.|\?|\!/)
+			.map(i => {
+				i = ' ' + i
+				let start = i.indexOf(' im ')
+				if (start === -1) {
+					return
+				}
+				return i.substr(start)
+			})
+			.filter(i => i)
+			.join(' and ')
 
-		let start 
-		if(modified) {
-			message.channel.send(`Hi ${modified.substr(start).split(' im ').map(i=>i.trim()).filter(i=>i).join(' ')}, I'm Dad!`);
+		let start
+		if (modified) {
+			message.channel.send(`Hi ${modified.substr(start).split(' im ').map(i => i.trim()).filter(i => i).join(' ')}, I'm Dad!`);
 		}
 
 		// if message starts with "!"
-		if (str.startsWith("!")) { 
+		if (str.startsWith("!")) {
 			// store the command for cleaner code/reading
-			let command = str.substring(1); 
+			let command = str.substring(1);
 			runCommand(command, message, str);
 		}
 	});
